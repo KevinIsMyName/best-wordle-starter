5523ec442b77d789a1be56dd92abc6a1c952c854
KevinIsMyName <wu.kevin.2520@gmail.com>
2022-02-24 23:48:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# best-wordle-starter
+
+This just shows you what the best words are. It's not done really well since it just ranks all the words based on how common each character is.
+
+This does not account for "yellow characters" that are in the word but incorrectly placed. It shows you what the best starting word is to get "green characters"
+
+[Wordle answers](wordle-answers-alphabetical.txt) sourced from: [wordle-answers-alphabetical.txt](https://gist.github.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b)  
+[Wordle allowed guesses](wordle-allowed-guesses.txt) sourced from: [wordle-allowed-guesses.txt](https://gist.github.com/cfreshman/cdcdf777450c5b5301e439061d29694c)
---
+++ main.py
@@ -0,0 +1,54 @@
+FILENAME = "wordle-allowed-guesses.txt"  # File to read in
+
+# Initialize our data structs
+occurences_a = {}
+occurences_b = {}
+occurences_c = {}
+occurences_d = {}
+occurences_e = {}
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+for char in list(alphabet):
+    occurences_a[char] = 0
+    occurences_b[char] = 0
+    occurences_c[char] = 0
+    occurences_d[char] = 0
+    occurences_e[char] = 0
+
+# Populate frequency for each char pos
+with open(FILENAME) as f:
+    lines = f.readlines()
+    for line in lines:
+        word = line.strip()
+        chars = list(word)
+        a, b, c, d, e = chars
+        occurences_a[a] += 1
+        occurences_b[b] += 1
+        occurences_c[c] += 1
+        occurences_d[d] += 1
+        occurences_e[e] += 1
+
+# Calculate word scores
+scores = {}
+with open(FILENAME) as f:
+    lines = f.readlines()
+    for line in lines:
+        word = line.strip()
+        chars = list(word)
+        a, b, c, d, e = chars
+        score = (
+            occurences_a[a]
+            + occurences_b[b]
+            + occurences_c[c]
+            + occurences_d[d]
+            + occurences_e[e]
+        )  # Word score is just sum of each character's frequencies
+        scores[word] = score
+
+# Print our possible words, ascending.
+# https://stackoverflow.com/questions/11228812/print-a-dict-sorted-by-values
+d = scores
+for v in sorted(d.values()):
+    for key in d:
+        if d[key] == v:
+            print(key, v)
+            break
